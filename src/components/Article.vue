b397e13d5eea8dcfa37f685a412059b7fcebcce7
Pam Osborn Popp <pamosbornpopp@gmail.com>
2025-05-30 13:30:30 -0500

    Update article text and add plant images

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,19 +1,321 @@
 <template>
   <div class="container">
-    <h2>Native Plants in Tennessee</h2>
+    <!-- <h2>Native Plants in Tennessee</h2> -->
     <!-- <iframe
       :src="articleURL"
       width="100%"
       height="500px"
       style="border: 1px solid #ccc; border-radius: 8px"
     ></iframe> -->
-    <div class="article-content">
-      <h3>Article Title</h3>
-      <p>Abstract: This is a brief summary of the article.</p>
-      <p>Introduction: This is the introduction to the article.</p>
-      <p>Methods: This section describes the methods used in the study.</p>
-      <p>Results: This section presents the results of the study.</p>
-    </div>
+    <article>
+      <h1>Tennessee Native Plants</h1>
+
+      <section>
+        <p>
+          Native plants have a lot of unknown effects on the environment that
+          many people miss when planting and gardening. Some of the effects that
+          native plants have on the environment are:
+        </p>
+        <ul>
+          <li>
+            Filtration: Native plants are accustomed to the quality and acidity
+            of the water in their habitat. This helps keep the water quality
+            consistent or higher as the plants remove pollutants.
+          </li>
+          <li>
+            Erosion: As more forests are cleared for housing and roadways, the
+            soil becomes loose, leading to erosion and soil washout. Native
+            plants help to prevent erosion by having roots that hold those soils
+            like deep roots for lots of dirt and looser clay soil, and shorter
+            roots for more rocky soils.
+          </li>
+          <li>
+            Controlling Animal Populations: Native plants help control animal
+            populations because as non-native plants take over the forest and
+            native plants become limited, animal populations begin to either
+            overpopulate due to an abundance of food or will not have enough
+            food sources and begin to become less populated. Invasive plants
+            disrupt the ecosystem while native plants keep it balanced.
+          </li>
+        </ul>
+        <p>
+          As people begin to plant non native plants and begin to defer from
+          using native plants, the biodiversity begins to shrink. Tennessee has
+          one of the best biodiverse ecosystems in the United States due to a
+          wide range of environments from wetland areas and swamps in the
+          Mississippi Valley and lots of rivers and creek systems feeding into
+          large lakes, to mountainous regions in eastern TN following the
+          Appalachian Mountains and Cumberland Mountains, and lots of hardwood
+          forests throughout TN that hold many different habitats like cedar
+          glades, pine forests, and deciduous forests.
+        </p>
+      </section>
+
+      <section>
+        <h2>Tennessee Wetlands</h2>
+        <p>
+          Having native plants in wetland areas creates a very advanced
+          filtration system and habitat for fish or other aquatic life. One way
+          that the filtration system is much better in native plants is because
+          the roots and stalks of the plants are tuned to the environment,
+          allowing them to take out chemicals in the water that could be harmful
+          or lead to algal blooms. The wetland plants also create a large range
+          of habitats for aquatic life because they can be like a tree providing
+          structure for fish to hide and mussels to grow on, or grasses that
+          provide food for herbivorous life, and help erosion from happening as
+          waves hit the shoreline and slowly wipe away the soil.
+        </p>
+
+        <h3>Narrowleaf Cattail (<em>Typha angustifolia</em>)</h3>
+        <img
+          src="/plant-learning-study/images/plantnames/narrow leaf cattail.jpg"
+          alt="Narrowleaf Cattail"
+        />
+        <img
+          src="/plant-learning-study/images/plantnames/narrow leaf cattail image.jpg"
+          alt="Narrowleaf Cattail"
+        />
+        <ul>
+          <li>Occurs in almost all of North America</li>
+          <li>
+            Lives in any consistently wet areas like lakes, bogs, and even damp
+            roadsides
+          </li>
+          <li>Grows about 4-6 feet high</li>
+          <li>Each plant has about ten leaves and only one flower</li>
+          <li>Flowering leaf is shorter than the other leaves</li>
+          <li>Entire plant is edible, but tastes better cooked</li>
+        </ul>
+
+        <h3>Black Chokeberry (<em>Aronia melanocarpa</em>)</h3>
+        <img
+          src="/plant-learning-study/images/plantnames/black chokeberry fruit.png"
+          alt="Black Chokeberry Fruit"
+        />
+        <img
+          src="/plant-learning-study/images/plantnames/black chokeberry flower.png"
+          alt="Black Chokeberry Flower"
+        />
+        <ul>
+          <li>Native to eastern North America</li>
+          <li>Lives in swampy bog-like areas, along with low woodland areas</li>
+          <li>Produces a black fruit that is red during the autumn</li>
+          <li>Fruit is edible</li>
+          <li>Has a white and pink flower</li>
+          <li>Is a deciduous flowering plant</li>
+        </ul>
+
+        <h3>Water Tupelo (<em>Nyssa aquatica</em>)</h3>
+        <img
+          src="/plant-learning-study/images/plantnames/water tupelo trunk.png"
+          alt="Water Tupelo Trunk"
+        />
+        <img
+          src="/plant-learning-study/images/plantnames/water tupelo leafs.png"
+          alt="Water Tupelo Leaves"
+        />
+        <ul>
+          <li>Native to the Mississippi River Basin and East Coast Marshes</li>
+          <li>Lives in marshes and wetlands</li>
+          <li>Produces a black/green oval-shaped fruit</li>
+          <li>The fruit pulp is edible</li>
+          <li>Is a deciduous tree</li>
+        </ul>
+
+        <h3>Marsh Marigold (<em>Caltha palustris</em>)</h3>
+        <img
+          src="/plant-learning-study/images/plantnames/marsh marigold plant.png"
+          alt="Marsh Marigold Plant"
+        />
+        <img
+          src="/plant-learning-study/images/plantnames/marsh marigold flower.png"
+          alt="Marsh Marigold Flower"
+        />
+        <ul>
+          <li>Native to the Northern Hemisphere</li>
+          <li>
+            Lives in marshes, fens, and wet woodlands in temperate regions
+          </li>
+          <li>Has a yellow 5-petaled flower that blooms in April and August</li>
+          <li>Is a perennial flowering plant</li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>Eastern TN Mountains</h2>
+        <p>
+          The eastern TN plants that grow in the Appalachian Mountains and the
+          Great Smoky Mountains National Park are known for their amazing fall
+          colors and beautiful spring blooms. The many streams throughout the
+          mountains provide water full of minerals to many different plant
+          species. This leaves the Great Smoky Mountains National Park with the
+          most biodiversity in the United States. Within this biodiversity,
+          there are many known plants like the rhododendrons, the towering oak
+          trees, and the pine trees that are green all year long. There are also
+          lesser-known plants that play just as important roles providing
+          nutrients to the fauna and improving the spring and summer beauty of
+          the mountains.
+        </p>
+
+        <h3>Smooth Blackberry (<em>Rubus canadensis</em>)</h3>
+        <img
+          src="/plant-learning-study/images/plantnames/smooth blackberry.png"
+          alt="Smooth Blackberry"
+        />
+        <img
+          src="/plant-learning-study/images/plantnames/Smooth blackberry flower.png"
+          alt="Smooth Blackberry flower"
+        />
+        <ul>
+          <li>Native to Central and Eastern US, along with Canada</li>
+          <li>Lives in a forested habitat</li>
+          <li>Produces a black fruit that is red when it isn’t fully mature</li>
+          <li>Has a white flower that is in the rose family</li>
+          <li>Fruit is edible</li>
+        </ul>
+
+        <h3>Lowbush Blueberry (<em>Vaccinium angustifolium</em>)</h3>
+        <img
+          src="/plant-learning-study/images/plantnames/lowbush blueberry.png"
+          alt="Lowbush Blueberry"
+        />
+        <img
+          src="/plant-learning-study/images/plantnames/lowbush blueberry flower.png"
+          alt="Lowbush Blueberry"
+        />
+        <ul>
+          <li>Native to Central and Eastern US, along with Canada</li>
+          <li>Lives in open woody areas, fields, and rocky balds</li>
+          <li>Produces a dark blue berry</li>
+          <li>Has a whitish-pink bell-shaped flower</li>
+          <li>The berry of the plant is edible</li>
+        </ul>
+
+        <h3>Silverleaf Mountain Mint (<em>Pycanthemum incanum</em>)</h3>
+        <img
+          src="/plant-learning-study/images/plantnames/silver leaf mountain mint.png"
+          alt="Silverleaf Mountain Mint"
+        />
+        <ul>
+          <li>
+            Native to Eastern US down the Appalachian Mountains and into
+            Ontario, Canada
+          </li>
+          <li>Lives in woodland areas with rocky soil</li>
+          <li>Is an herbaceous plant in the mint family</li>
+          <li>Perennial flowering plant</li>
+          <li>Leaves are edible and made into teas when crushed</li>
+        </ul>
+
+        <h3>Little Sweet Betsy (<em>Trillium cuneatum</em>)</h3>
+        <img
+          src="/plant-learning-study/images/plantnames/Little_sweet betsy.png"
+          alt="Little Sweet Betsy"
+        />
+        <img
+          src="/plant-learning-study/images/plantnames/little sweet_betsy.png"
+          alt="Little Sweet Betsy"
+        />
+        <ul>
+          <li>
+            Native to the southeastern US and lives in upland woodlands and the
+            Appalachian Mountains
+          </li>
+          <li>
+            Has a reddish-purple flower with three leaves, patterned with light
+            and dark green
+          </li>
+          <li>Is not edible</li>
+          <li>
+            A rhizome flowering plant (a subterranean plant that sends shoots
+            and sprouts from nodes)
+          </li>
+          <li>Plant and flowers are perennial</li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>Tennessee Woodlands</h2>
+        <p>
+          The TN hardwood forests contain many different plant species. Some of
+          these species are only found here in Tennessee. These species can be
+          found in many environments, like cedar glades. Cedar glades are mostly
+          found in Tennessee due to the rocky terrain, which allows cedar trees,
+          lichens, mosses, and wildflowers to grow. Along with these cedar
+          glades, there are lots of deciduous forests that have many different
+          plants and animals, some could still be unknown.
+        </p>
+
+        <h3>Eastern Red Cedar (<em>Juniperus virginiana</em>)</h3>
+        <img
+          src="/plant-learning-study/images/plantnames/eastern_red cedar.png"
+          alt="Eastern Red Cedar"
+        />
+        <img
+          src="/plant-learning-study/images/plantnames/eastern red cedar berry.png"
+          alt="Eastern Red Cedar Berry"
+        />
+        <ul>
+          <li>Native to central and eastern US</li>
+          <li>Lives in cedar glades, limestone hills, and prairies</li>
+          <li>Produces a blue seed</li>
+          <li>The seed is edible, but too many can lead to illness</li>
+          <li>Is a coniferous evergreen.</li>
+        </ul>
+
+        <h3>Tennessee Coneflower (<em>Echinacea tennesseensis</em>)</h3>
+        <img
+          src="/plant-learning-study/images/plantnames/TN Coneflower.png"
+          alt="Tennessee Coneflower"
+        />
+        <ul>
+          <li>Native to the cedar glades of Tennessee</li>
+          <li>Lives in rocky pastures</li>
+          <li>
+            Produces a purple flower that surrounds a brownish seed-filled
+            center
+          </li>
+          <li>Is a perennial flowering plant</li>
+          <li>Is not edible</li>
+        </ul>
+
+        <h3>Wild Strawberry (<em>Fragaria virginiana</em>)</h3>
+        <img
+          src="/plant-learning-study/images/plantnames/wild strawberry.jpg"
+          alt="Wild Strawberry"
+        />
+        <ul>
+          <li>Native to almost all of North America</li>
+          <li>Lives in wooded forests and along field and forest edges</li>
+          <li>
+            Produces a red berry that is produced from the pistil of a white
+            flower
+          </li>
+          <li>The fruit is edible</li>
+          <li>The plant is deciduous</li>
+        </ul>
+
+        <h3>Black Walnut (<em>Juglans nigra</em>)</h3>
+        <img
+          src="/plant-learning-study/images/plantnames/black walnut.png"
+          alt="Black Walnut"
+        />
+        <img
+          src="/plant-learning-study/images/plantnames/black walnut leafs.png"
+          alt="Black Walnut"
+        />
+        <ul>
+          <li>Native to Eastern US</li>
+          <li>Lives in hardwood forests</li>
+          <li>
+            Produces a spherical nut that has a brownish-green husk that is
+            fleshy and holds a brown corrugated nut
+          </li>
+          <li>Is a deciduous tree</li>
+        </ul>
+      </section>
+    </article>
+
     <div class="time-remaining">
       <p>Time remaining:</p>
       <div class="progress-wrapper">
@@ -28,8 +330,8 @@ export default {
   data() {
     return {
       // articleURL: "https://docs.google.com/document/d/your-doc-id/preview", // or published link
-      totalTime: 6, // in seconds
-      timeLeft: 6, // 10 minutes
+      totalTime: 600, // in seconds // 10 minutes = 600 seconds // TODO: Update to 600 seconds after testing
+      timeLeft: 600, // 10 minutes
     };
   },
   computed: {
@@ -73,5 +375,10 @@ export default {
   font-size: 0.95rem;
   font-weight: 500;
   color: #555;
+}
+
+img {
+  max-width: 300px;
+  height: auto;
 }
 </style>
